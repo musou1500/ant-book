922ef8eac13f9933063a06e50c7992cd7120fb50
musou1500 <musou1500@gmail.com>
2019-05-14 02:10:39 +0900

    部分和問題を解いた

---
+++ q2_1_2.py
@@ -0,0 +1,16 @@
+from sys import stdin
+
+def solve(s, nums, idx = 0):
+    if idx == len(nums):
+        return s == 0
+    else:
+        return solve(s - nums[idx], nums, idx + 1) or solve(s, nums, idx + 1)
+
+def main():
+    lines = stdin.readlines()
+    s = int(lines[0])
+    nums = list(map(lambda n: int(n), lines[1].split(' ')))
+    print('Yes' if solve(s, nums) else 'No')
+
+if __name__ == '__main__':
+    main()
